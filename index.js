bd3dfc8f97b47b9671c361226d4b9553dfea5bf1
Luke Bedrosian <bedrosian.luke05@gmail.com>
2021-10-14 13:37:06 -0400

    commit tree f4274545799d0e2aa735141ef7cf550da69aca52

--- index.js
+++ index.js
@@ -1,5 +1,12 @@
 function hasTargetSum(array, target) {
   // Write your algorithm here
+for (let i = 0; i < array.length; i++) {
+  const complement = target - array[i]
+  for (let j = i + 1; j < array.length; j++) {
+    if (array[j] === complement) return true
+  }
+}
+    return false;
 }
 
 /* 
@@ -8,10 +15,17 @@ function hasTargetSum(array, target) {
 
 /* 
   Add your pseudocode here
+  comp = targeted number - array 0
+  see if any numbers are comp
+  if so, return true
+  if non are, return false
 */
+
+//if any two numbers equal the targeted number, return true
 
 /*
   Add written explanation of your solution here
+
 */
 
 // You can run `node index.js` to view these console logs
@@ -32,3 +46,14 @@ if (require.main === module) {
 }
 
 module.exports = hasTargetSum;
+
+/*
+hasTargetSum([3, 8, 12, 4, 11, 7], 10);
+// returns true, since 3 and 7 add up to 10
+
+hasTargetSum([22, 19, 4, 6, 30], 25);
+// returns true, since 19 and 6 add up to 25
+
+hasTargetSum([1, 2, 5], 4);
+// returns false, since no pair of numbers adds up to 4
+*/
